7750970a0f79d402f59090883119edf3bb6a4fcf
Mac <jon_markov@sina.com>
2025-03-31 16:29:59 +0800

    添加子账号

---
+++ pages/system/UserAuthSys/index.js
@@ -0,0 +1,77 @@
+const {
+  _handleWindowInfo,
+  _handleDeviceInfo
+} = require('../../../utils/public').default
+Page({
+  data: {
+    c_screen_height: _handleWindowInfo.screenHeight || 0, //全高度
+    c_statusBarHeight: _handleWindowInfo.statusBarHeight || 0, // 状态栏高度
+    c_navBarHeight: _handleDeviceInfo.platform == 'ios' ? 49 : 44, // 导航栏高度，默认值
+    s_platform_height: _handleDeviceInfo.platform == "ios" || _handleDeviceInfo.platform == "devtools" ? 95 : 60, //判断系统获取底部高度
+    s_background_picture_of_the_front_page: '', //背景
+    params: {},
+    eye_show_hide: true
+  },
+
+  initialiImageBaseConversion() {
+    const _this = this;
+    const imageMap = [{
+      path: '/assets/images/home/car-bg.png',
+      key: 's_background_picture_of_the_front_page'
+    }];
+    const promises = imageMap.map(item =>
+      new Promise((resolve, reject) => {
+        wx.getFileSystemManager().readFile({
+          filePath: item.path,
+          encoding: 'base64',
+          success: (res) => {
+            resolve({
+              [item.key]: `data:image/png;base64,${res.data}`
+            });
+          }
+        });
+      })
+    );
+
+    Promise.all(promises)
+      .then(results => {
+        const dataToUpdate = results.reduce((acc, curr) => ({
+          ...acc,
+          ...curr
+        }), {});
+        _this.setData(dataToUpdate);
+      });
+  },
+  // 内容输入回调
+  handleBindinput(evt) {
+    const {
+      params
+    } = this.data
+    params[evt.currentTarget.dataset.item] = evt.detail.value
+    this.setData({
+      params: {
+        ...params
+      }
+    })
+  },
+  handleshowHide() {
+    const eye = this.data.eye_show_hide
+    this.setData({
+      eye_show_hide: !eye
+    })
+  },
+  onLoad(options) {
+
+  },
+
+  onReady() {
+    this.initialiImageBaseConversion()
+  },
+
+
+  onShow() {
+
+  },
+
+
+})
