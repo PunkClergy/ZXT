fac114a74c185295754f74e7d8e35662fabd594f
Mac <jon_markov@sina.com>
2025-04-17 10:28:40 +0800

    编辑功能

--- pages/orderList/orderAdd/orderAdd.js
+++ pages/orderList/orderAdd/orderAdd.js
@@ -95,6 +95,8 @@ Page({
         const resp = response.data.content
         this.setData({
           g_product_type_list: resp
+        }, () => {
+          this.initReplaceIndex()
         })
       });
   },
@@ -114,6 +116,8 @@ Page({
         const resp = response.data.content
         this.setData({
           g_country_list: resp
+        }, () => {
+          this.initReplaceIndex()
         })
       });
   },
@@ -135,6 +139,8 @@ Page({
         const resp = response.data.content
         this.setData({
           g_device_version_list: resp
+        }, () => {
+          this.initReplaceIndex()
         })
       });
   },
@@ -157,7 +163,7 @@ Page({
               ele => ele.id === g_device_type_id
             );
             this.setData({
-              g_device_type_index:g_device_type_index==-1?null:g_device_type_index
+              g_device_type_index: g_device_type_index == -1 ? null : g_device_type_index
             })
           }
         })
@@ -519,25 +525,41 @@ Page({
   },
   // 寻找索引值
   initReplaceIndex() {
-    const { 
+    console.log(this.data)
+    const {
       g_product_type_list = [],
-      g_product_type_id,
-      g_country_list = [],
-      g_country_id,
-      g_device_version_list = [],
-      g_device_version_id
+        g_product_type_id,
+        g_country_list = [],
+        g_country_id,
+        g_device_version_list = [],
+        g_device_version_id
     } = this.data;
     const findValidIndex = (list, targetId) => {
       if (!Array.isArray(list)) return null;
       const index = list.findIndex(item => item.id === targetId);
       return index >= 0 ? index : null;
     };
-    const indexConfigs = [
-      { key: 'g_product_type_index', list: g_product_type_list, id: g_product_type_id },
-      { key: 'g_country_index', list: g_country_list, id: g_country_id },
-      { key: 'g_device_version_index', list: g_device_version_list, id: g_device_version_id }
+    const indexConfigs = [{
+        key: 'g_product_type_index',
+        list: g_product_type_list,
+        id: g_product_type_id
+      },
+      {
+        key: 'g_country_index',
+        list: g_country_list,
+        id: g_country_id
+      },
+      {
+        key: 'g_device_version_index',
+        list: g_device_version_list,
+        id: g_device_version_id
+      }
     ];
-    const updateData = indexConfigs.reduce((acc, { key, list, id }) => {
+    const updateData = indexConfigs.reduce((acc, {
+      key,
+      list,
+      id
+    }) => {
       acc[key] = findValidIndex(list, id);
       return acc;
     }, {});
@@ -548,13 +570,15 @@ Page({
     });
   },
   onLoad(options) {
+    console.log(options)
+    console.log(options.item)
     if (options.item) {
       this.initOptions(JSON.parse(options.item))
     }
   },
 
   onReady() {
-    this.initReplaceIndex()
+
     if (!this.data.snitems) {
       this.handleChoiceStorage()
     }
