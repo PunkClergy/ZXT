b7cbfb011abc2c3908b2471f2240d7015b13ffe4
Mac <jon_markov@sina.com>
2025-04-21 17:22:45 +0800

    Update index.js

--- pages/channel/placeAddWork/index.js
+++ pages/channel/placeAddWork/index.js
@@ -114,7 +114,6 @@ Page({
       [u_dispatchWork.bdId]: companyType == 2 ? select_content_bddata?.id : ''
     }
     byPost(getApp().data.k1swUrl + u_dispatchWork.URL, requestParam, (response) => {
-      console.log(response)
       wx.removeStorage({
         key: 'guestdata',
         success() {}
