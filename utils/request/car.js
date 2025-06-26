b928c99e6051496864d34fa9d116d79060a507da
Mac <jon_markov@sina.com>
2025-06-26 15:33:17 +0800

    财务保管原车钥匙

--- utils/request/car.js
+++ utils/request/car.js
@@ -12,6 +12,10 @@ const u_carList = {
 const u_addOrUpdateCar = {
   URL: fin3plusUrl + "carapi/addOrUpdateCar",
 }
+// 新增车辆钥匙
+const u_safekeyapiaddOrUpdate = {
+  URL: fin3plusUrl + "safekeyapi/addOrUpdate",
+}
 // 车务人员列表
 const u_carManagerapi_list = {
   URL: fin3plusUrl + "carManagerapi/list",
@@ -34,12 +38,17 @@ const u_wycRentVehicleList = {
   URL: fin3plusUrl + "/rentKeyApi/wycRentVehicleList",
   page: 'page'
 }
-
+const u_safekeyapiList = {
+  URL: fin3plusUrl + "/safekeyapi/list",
+  page: 'page'
+}
 module.exports = {
+  u_safekeyapiaddOrUpdate,
   u_wycRentVehicleList,
   u_carManagerapi_del,
   u_carManagerapi_addOrUpdate,
   u_carManagerapi_list,
   u_carList,
-  u_addOrUpdateCar
+  u_addOrUpdateCar,
+  u_safekeyapiList
 }
