8997db5814cb7d00ae8ad71a9d985da51feb65a1
PunkMonk <43631789+PunkClergy@users.noreply.github.com>
2025-07-14 20:02:14 +0800

    删除

--- pages/orderList/InquirySheet/index.js
+++ pages/orderList/InquirySheet/index.js
@@ -1,154 +1,66 @@
-const {
-  _handleWindowInfo,
-  _handleDeviceInfo
-} = require('../../../utils/public').default
-const {
-  showLoading,
-  hideLoading,
-  showToast
-} = require('../../../utils/Inspect/tips')
-const {
-  byPost,
-  byGet
-} = require('../../../utils/request/http')
-const {
-  u_getIndustry,
-  u_getIntroduction,
-  u_saleCode
-} = require('../../../utils/request/data_info')
-
+// pages/orderList/InquirySheet/index.js
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    c_screen_height: _handleWindowInfo.screenHeight || 0, //全高度
-    c_statusBarHeight: _handleWindowInfo.statusBarHeight || 0, // 状态栏高度
-    c_navBarHeight: _handleDeviceInfo.platform == 'ios' ? 49 : 44, // 导航栏高度，默认值
-    s_platform_height: _handleDeviceInfo.platform == "ios" || _handleDeviceInfo.platform == "devtools" ? 95 : 60, //判断系统获取底部高度
-    s_background_picture_of_the_front_page: '', //背景
-    params: {},
-    g_industry: [], //所属行业
-    g_industry_index: null, //当前行业
-    g_core_functions: [], //所属功能
-    g_core_functions_index: null, //当前功能
-    g_device_type_list: [], //销售代号
-    g_device_type_index: null, //当前销售代号
-  },
-  // 全屏背景
-  initialiImageBaseConversion() {
-    const _this = this;
-    const imageMap = [{
-      path: '/assets/images/home/car-bg.png',
-      key: 's_background_picture_of_the_front_page'
-    }];
-    const promises = imageMap.map(item =>
-      new Promise((resolve, reject) => {
-        wx.getFileSystemManager().readFile({
-          filePath: item.path,
-          encoding: 'base64',
-          success: (res) => {
-            resolve({
-              [item.key]: `data:image/png;base64,${res.data}`
-            });
-          }
-        });
-      })
-    );
-
-    Promise.all(promises)
-      .then(results => {
-        const dataToUpdate = results.reduce((acc, curr) => ({
-          ...acc,
-          ...curr
-        }), {});
-        _this.setData(dataToUpdate);
-      });
-  },
-  initialiIndustry() {
-    byGet(getApp().data.k1swUrl + u_getIndustry.URL, {}).then(response => {
-      const list = response.data.content
-      const info = list.map(ele => {
-        let temp = {
-          id: ele,
-          name: ele
-        }
-        return temp
-      })
-      this.setData({
-        g_industry: info
-      })
-    })
-  },
-  initialgetIntroduction() {
-    byGet(getApp().data.k1swUrl + u_getIntroduction.URL, {}).then(response => {
-      const list = response.data.content
-      const info = list.map(ele => {
-        let temp = {
-          id: ele,
-          name: ele
-        }
-        return temp
-      })
-      this.setData({
-        g_core_functions: info
-      })
-    })
-  },
-  handleIndustryType(evt) {
-    this.setData({
-      g_industry_index: evt.detail.value
-    }, () => {
-      this.handleCalculate()
-    })
-  },
-  handleCoreType(evt) {
-    this.setData({
-      g_core_functions_index: evt.detail.value
-    }, () => {
-      this.handleCalculate()
-    })
-  },
-  handleCalculate() {
-    const {
-      g_core_functions_index,
-      g_industry_index,
-      g_industry,
-      g_core_functions
-    } = this.data
-    if (g_industry_index == null || g_core_functions_index == null) return
-    const parmas = {
-      introduction: g_core_functions[g_core_functions_index]?.name,
-      industry: g_industry[g_industry_index]?.name
-    }
-    byGet(getApp().data.k1swUrl + u_saleCode.URL, parmas).then(response => {
-      const list = response.data.content
-      console.log(list)
-      this.setData({
-        g_device_type_list: list
-      })
-
-    })
 
   },
-  handleSubmit() {
-    wx.reLaunch({
-      url: '/pages/orderList/orderAdd/orderAdd',
-    })
-  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
   onLoad(options) {
-    this.initialiIndustry()
-    this.initialgetIntroduction()
+
   },
-  onReady() {},
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
   onShow() {
-    this.initialiImageBaseConversion()
+
   },
 
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
 
+  },
 
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
 
+  },
 
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
 
+  },
 
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
 
+  },
 
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
 
+  }
 })
