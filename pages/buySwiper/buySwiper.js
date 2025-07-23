b9aef4c804816cb72e132056ac2500ba715926d5
PunkMonk <43631789+PunkClergy@users.noreply.github.com>
2025-07-23 18:08:31 +0800

    Update buySwiper.js

--- pages/buySwiper/buySwiper.js
+++ pages/buySwiper/buySwiper.js
@@ -59,7 +59,7 @@ Page({
     const type = this.data.insurance_type
     wx.navigateToMiniProgram({
       appId: 'wxcdd55b1d2e790195', // 必填
-      path: `/pages/buySwiper/buySwiper?userId=${id}&type=${type}`, // 可为空，但建议填写
+      path: `/pages/index/index?userId=${id}&type=${type}`, // 可为空，但建议填写
       envVersion: 'release', // 可选值：develop（开发版），trial（体验版），release（正式版）
       success: function (res) {
         // 成功跳转的回调
