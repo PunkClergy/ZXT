25553828758dbd8bd682a73bf2e1652c75e2f1c5
PunkMonk <43631789+PunkClergy@users.noreply.github.com>
2025-07-31 18:36:51 +0800

    车辆设置

--- pages/listOfPrivateCars/index.js
+++ pages/listOfPrivateCars/index.js
@@ -4,7 +4,10 @@ const {
 } = require('../../utils/public').default
 Page({
   data: {
-
+    c_screen_height: _handleWindowInfo.screenHeight || 0, //屏幕高度
+    c_screen_width: _handleWindowInfo.windowWidth || 0, //屏幕宽度
+    statusBarHeight: _handleWindowInfo.statusBarHeight || 0, // 状态栏高度
+    navBarHeight: _handleDeviceInfo.platform == 'ios' ? 49 : 44, // 导航栏高度，默认值
   },
   // 全屏背景图
   initialiImageBaseConversion() {
@@ -37,9 +40,7 @@ Page({
       });
   },
   onLoad: function () {
-
   },
-
   onShow() {
     this.initialiImageBaseConversion()
   },
