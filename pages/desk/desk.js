485b4e835c7348f54c07e1bddc0d8399251692ac
PunkMonk <43631789+PunkClergy@users.noreply.github.com>
2025-10-31 20:27:39 +0800

    弹窗

--- pages/desk/desk.js
+++ pages/desk/desk.js
@@ -2,6 +2,7 @@ const {
   _handleWindowInfo,
   _handleDeviceInfo
 } = require('../../utils/public').default
+const appUtil = require('../../utils/app-util.js');
 const {
   u_bannerlist,
   u_midMenulist,
@@ -13,7 +14,8 @@ const {
   u_updateUserName,
   u_getQrcodeImg,
   u_getNotHaveMidMenulist,
-  u_applyMenus
+  u_applyMenus,
+  u_setBtype
 } = require('../../utils/request/home')
 
 const {
@@ -60,12 +62,18 @@ Page({
     account: null,
     special_area_modal: false,// 选择专区弹窗
     join_the_group_modal: false,// 入群二维码弹窗
+    showLoginPopup: false,//登录弹窗
     selected: [],
     areaOptions: [] // 动态生成带长度标记的选项
   },
+  handleShowContact() {
+    this.setData({
+      join_the_group_modal: true
+    })
+  },
   // 请求更多功能数据
   initMoreData() {
-    byGet(getApp().data.k1swUrl + u_getNotHaveMidMenulist.URL, {}).then(response => {
+    byGet((getApp().data.k1swUrl || this.data.c_link) + u_getNotHaveMidMenulist.URL, {}).then(response => {
       const rawOptions = response?.data?.content
       const optionsWithLength = rawOptions.map(item => ({
         ...item,
@@ -90,7 +98,8 @@ Page({
   },
   // 请求入群码
   initQrCode() {
-    byGet(getApp().data.k1swUrl + u_getQrcodeImg.URL, {}).then(response => {
+    byGet((getApp().data.k1swUrl || this.data.c_link) + u_getQrcodeImg.URL, {}).then(response => {
+      console.log(response, '1111')
       if (response.statusCode == 200) {
         this.setData({
           personal_qr_code: response.data.content.img
@@ -393,20 +402,21 @@ Page({
         this.setData({
           special_area_modal: false,
         }, () => {
-          wx.showModal({
-            title: '申请成功',
-            content: '恭喜您申请成功！请加入专属客服群，并联系管理员完成权限审批。',
-            showCancel: false, 
-            confirmText: '我知道了',
-            success: (res) => {
-              if (res.confirm) {
-                this.setData({
-                  join_the_group_modal: true,
-                  selected: []
-                });
-              }
-            }
-          });
+          this.handleTermialList()
+          // wx.showModal({
+          //   title: '申请成功',
+          //   content: '恭喜您申请成功！请加入专属客服群，并联系管理员完成权限审批。',
+          //   showCancel: false,
+          //   confirmText: '我知道了',
+          //   success: (res) => {
+          //     if (res.confirm) {
+          //       this.setData({
+          //         join_the_group_modal: true,
+          //         selected: []
+          //       });
+          //     }
+          //   }
+          // });
         })
       }
     });
@@ -419,7 +429,13 @@ Page({
   handleQRClose() {
     console.log(111)
     this.setData({
-      join_the_group_modal: false
+      join_the_group_modal: false,
+    }, () => {
+      if (!isLogin()) {
+        this.setData({
+          showLoginPopup: true
+        })
+      }
     })
   },
   // 判断当前为三行排列还是单行排列
@@ -607,6 +623,12 @@ Page({
       });
     })
   },
+  handleConsult() {
+    this.setData({
+      join_the_group_modal: true,
+      showLoginPopup: false
+    })
+  },
   initLogo() {
     const _this = this
     byGet(_this.data.c_link + u_logo.URL, {}).then(response => {
@@ -619,6 +641,121 @@ Page({
         logoSrc
       });
     })
+  },
+  async onGetPhoneNumber(e) {
+    console.log(e)
+    try {
+      // 获取登录凭证
+      const loginRes = await new Promise((resolve, reject) => {
+        wx.login({
+          success: resolve,
+          fail: (err) => reject(new Error(`登录失败: ${err.errMsg}`))
+        });
+      });
+
+      if (!loginRes.code) {
+        throw new Error('无法获取登录凭证');
+      }
+
+      // 检查授权码
+      if (!e.detail?.code) {
+        console.warn('用户拒绝了授权');
+        return;
+      }
+
+      // 发送登录请求
+      const response = await new Promise((resolve, reject) => {
+        byPost(
+          `${this.data.c_link}userapi/wxLogin`,
+          {
+            code: e.detail.code,
+            inviteCode: this.data.invit_code || '',
+            wxCode: loginRes.code
+          },
+          (res) => {
+            if (res?.data?.content) {
+              resolve(res);
+            } else {
+              reject(new Error(res?.data?.message || '登录接口响应异常'));
+            }
+          },
+          (err) => reject(new Error(`网络请求失败: ${err.errMsg}`))
+        );
+      });
+
+      const userInfo = response.data.content;
+      if (!userInfo) {
+        throw new Error('用户信息获取失败');
+      }
+
+      // 配置URL
+      const isTestUser = userInfo.username === '13683187039*';
+      const urlConfig = {
+        k1swUrl: isTestUser
+          ? 'https://k1swtest.wiselink.net.cn/'
+          : 'https://k3a.wiselink.net.cn/',
+        fin3Url: 'https://fin3.wiselink.net.cn/fin/' // 固定地址
+      };
+
+      // 批量存储数据
+      const app = getApp();
+      const storageTasks = [
+        [app.data.k1swUrlKey, urlConfig.k1swUrl],
+        [app.data.fin3UrlKey, urlConfig.fin3Url],
+        [app.data.userKey, userInfo]
+      ].map(([key, value]) => new Promise((resolve, reject) => {
+        appUtil.setStorage(key, value, (success) =>
+          success ? resolve() : reject(`存储失败: ${key}`)
+        );
+      }));
+
+      await Promise.all(storageTasks);
+
+      // 更新应用数据
+      app.data.k1swUrl = urlConfig.k1swUrl;
+      app.data.fin3Url = urlConfig.fin3Url;
+      app.data.userInfo = userInfo;
+      app.data.reflag = 1;
+      console.log(response?.data)
+
+      if (!response?.data?.content?.btype) {
+        this.setData({
+          showLoginPopup: false,
+          btypeM: true
+        })
+      } else {
+        this.setData({
+          showLoginPopup: false,
+        })
+      }
+      return
+    } catch (error) {
+      console.error('处理流程异常:', error);
+      const errorMsg = error.message.includes('存储失败')
+        ? '本地数据处理失败，请重新登录！'
+        : '操作失败，请检查网络后重试';
+
+      appUtil.showModal(errorMsg, false, () => {
+        /* 可添加重试逻辑 */
+      });
+    }
+  },
+  handleSelectOption(e) {
+    const value = parseInt(e.currentTarget.dataset.value)
+    this.setData({
+      selectedqu: value
+    })
+  },
+  handleConfirmSelect() {
+    if (!this.data.selectedqu) return
+    byPost(this.data.c_link + u_setBtype.URL, { btype: this.data.selected }, (res) => {
+      console.log(res)
+      if (res?.data?.code == 1000) {
+        this.setData({
+          btypeM: false
+        })
+      }
+    });
   },
   onLoad: function (options) {
     wx.hideTabBar();
@@ -641,8 +778,13 @@ Page({
         }
       });
     } else if (!isLogin()) {
-      wx.navigateTo({ url: '/pages/privateCar/index' });
+      this.setData({
+        showLoginPopup: true
+      })
+      // wx.navigateTo({ url: '/pages/privateCar/index' });
+      // 改变一个状态
     }
+    this.handleTermialList()
   },
   triggerChildEvent() {
     // / 使用 this.selectComponent('#myChild') 来获取子组件实例
@@ -677,7 +819,7 @@ Page({
 
   onShow: function (e) {
     this.initialGetBanner()
-    this.handleTermialList()
+
     this.initQrCode()
     this.initMoreData()
     if (getApp()?.data?.reflag == 1) {
